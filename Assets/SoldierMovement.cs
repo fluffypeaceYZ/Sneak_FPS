96c6439b22d439e7316762ff567362814a249a2a
fluffypeaceYZ <buzgata@yahoo.ca>
2016-10-23 12:30:06 -0400

    Movement Script and Soldier

---
+++ Assets/SoldierMovement.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoldierMovement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		transform.position += new Vector3(0.3f, 0, 0); 
+	
+	}
+
+	void OnTriggerEnter(Collider other) {
+
+		if(other.gameObject.tag == "Right")
+		{
+			transform.position += new Vector3(-0.3f, 0, 0);
+		}
+
+		if(other.gameObject.tag == "Left")
+		{
+			transform.position += new Vector3(0.3f, 0, 0);
+		}
+	
+	}
+}
