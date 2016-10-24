12fa04dc8353ecdcd5177b0712180112251cfa42
Unknown <buzgata@yahoo.ca>
2016-10-24 16:56:05 -0400

    Health Bar

---
+++ Assets/UIManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+	public Sprite[] healthBarArray;
+	public Image healthBar;
+
+	private int healthBarCount = 0;
+
+	void Start () 
+	{
+	
+	}
+	
+	void Update ()
+	{
+		if (Input.GetKeyDown (KeyCode.P))
+		{
+			TakeDamage ();
+		}
+	}
+
+	public void TakeDamage()
+	{
+		healthBarCount++;
+
+		healthBar.sprite = healthBarArray [healthBarCount];
+	}
+
+}
